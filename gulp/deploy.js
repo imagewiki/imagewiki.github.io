67c17ae5818b4b23c73fcfe4ff34f1a6804d5737
pedromartos <pedro@devlandia.net>
2016-05-02 21:01:37 +0000

    Update gulp deploy task to deploy to dev and prod gh-pages

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -6,7 +6,17 @@ var ghPages = require('gulp-gh-pages');
 var conf    = require('./conf');
 
 
-gulp.task('deploy', ['build'], function() {
+gulp.task('deploy:dev', ['build'], function() {
   return gulp.src(conf.paths.dist + '/**/*')
-    .pipe(ghPages());
+    .pipe(ghPages({
+      branch: 'master'
+    }));
+});
+
+gulp.task('deploy:prod', ['build'], function() {
+  return gulp.src(conf.paths.dist + '/**/*')
+    .pipe(ghPages({
+      remoteUrl: 'git@github.com:imagewiki/website.git',
+      branch: 'gh-pages'
+    }));
 });
